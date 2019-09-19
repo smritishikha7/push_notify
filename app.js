27ac5fd7c438c0adfa61402a78196e5f8e5a41f3
smriti <smriti@freightbro.com>
2019-09-19 11:42:38 +0530

    service worker connection

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 // const admin = require('firebase-admin')
 
 var config = {
-    apiKey: "AIzaSyDyL-MvifOCgPQApE-cfKgLKXAE2nSIONQ",
-    authDomain: "pushtrial-285df.firebaseapp.com",
-    databaseURL: "https://pushtrial-285df.firebaseio.com",
-    projectId: "pushtrial-285df",
-    storageBucket: "",
-    messagingSenderId: "603784342893",
-    appId: "1:603784342893:web:da921f50fd0f3fe22d7e64"
-    // credential: admin.credential.applicationDefault(),
-  };
-  firebase.initializeApp(config);
-  
-  const messaging = firebase.messaging();
-  messaging.requestPermission().then(function() {
-     //getToken(messaging);
-     console.log("have permisson");
-     
-     return messaging.getToken();
-  }).then(function(token){
-  console.log(token);
-  })
-.catch(function(err) {
-  console.log('Permission denied', err);
-});
+  apiKey: "AIzaSyDyL-MvifOCgPQApE-cfKgLKXAE2nSIONQ",
+  authDomain: "pushtrial-285df.firebaseapp.com",
+  databaseURL: "https://pushtrial-285df.firebaseio.com",
+  projectId: "pushtrial-285df",
+  storageBucket: "",
+  messagingSenderId: "603784342893",
+  appId: "1:603784342893:web:da921f50fd0f3fe22d7e64"
+  // credential: admin.credential.applicationDefault(),
+};
+firebase.initializeApp(config);
 
+const messaging = firebase.messaging();
+messaging.requestPermission().then(function () {
+    //getToken(messaging);
+    console.log("have permisson");
+
+    return messaging.getToken();
+  }).then(function (token) {
+    console.log(token);
+  })
+  .catch(function (err) {
+    console.log('Permission denied', err);
+  });
+
+if ("serviceWorker" in navigator) {
+  send().catch(err => console.log(err));
+}
+
+function send() {
+  navigator.serviceWorker.register('service-worker.js')
+    .then((registration) => {
+      messaging.useServiceWorker(registration);
+    });
+
+}
 // Notification.requestPermission().then((permission) => {
 //     console.log(permission);
-    
+
 //     if (permission === 'granted') {
 //       console.log('Notification permission granted.');
 //       // TODO(developer): Retrieve an Instance ID token for use with FCM.
@@ -36,9 +47,9 @@ var config = {
 //       console.log('Unable to get permission to notify.');
 //     }
 //   });
-  
 
 
-messaging.onMessage(function(payload){
-console.log('onMessage: ',payload);
+
+messaging.onMessage(function (payload) {
+  console.log('onMessage: ', payload);
 });
